5132911db7fd8fc096fa51f7ffcc8a5226866fda
Neywiny <dylanshekter@gmail.com>
2019-06-20 16:25:46 -0400

    Implemented MultiMerger, fixed comp.clearHistory()

--- DylComp.py
+++ DylComp.py
@@ -10,9 +10,10 @@ class Comparator:
         Optimization levels: will not optimize, store result, do abc association, do recursive association
         withComp: configures if the comparator will compare pos/pos neg/neg
     """
-    def __init__(self, objects: list = None, level: int = 3, rand: bool=False, withComp: bool=True):
+    def __init__(self, objects: list = None, level: int = 3, rand: bool=False, withComp: bool=True, seed=None):
         self.clearHistory()
         self.rand = rand
+        self.seed = seed
         self.withComp = withComp
         self.level: int = level
         self.compHistory = list()
@@ -29,6 +30,23 @@ class Comparator:
     def __call__(self, a, b):
         """returns a < b"""
         return self.compare(a,b)
+        
+    def record(self, vals):
+        for val in vals:
+            self.counts[val] += 1
+
+            #count minimum separations
+            for i, hist in enumerate(reversed(self.compHistory)):
+                if i < self.minSeps[val] and val in hist:
+                    self.minSeps[val] = i
+
+            if self.last:
+                if val in self.last:
+                    self.dupCount += 1
+
+        self.compHistory.append(vals[:])
+        self.last = vals[:]
+
     def compare(self, a, b) -> bool:
         """returns a < b"""
         if not self.rand and self.level == 0:
@@ -43,20 +61,10 @@ class Comparator:
             else:
                 #only gets it right 80% of the time
                 needComp = True
-                if self.rand:
-                    if a < len(self.objects) // 2: # a is neg dist
-                        aNeg = True
-                        aScore = self.neg[a]
-                    else: #a is pos dist
-                        aNeg = False
-                        aScore = self.pos[a - len(self.objects) // 2]
-
-                    if b < len(self.objects) // 2: # b is neg dist
-                        bNeg = True
-                        bScore = self.neg[b]
-                    else: #b is pos dist
-                        bNeg = False
-                        bScore = self.pos[b - len(self.objects) // 2]
+                if self.rand: # where we are for most things
+                    
+                    aScore, aNeg = self.getLatentScore(a)
+                    bScore, bNeg = self.getLatentScore(b)
 
                     if not self.withComp and (aNeg == bNeg): # no need to actually compare?
                         needComp = False
@@ -66,21 +74,8 @@ class Comparator:
                 else:
                     res: bool = a < b
                 if needComp:
-                    self.counts[a] += 1
-                    self.counts[b] += 1
-
-                    #count minimum separations
-                    for i,(compA, compB) in enumerate(reversed(self.compHistory)):
-                        if i < self.minSeps[a] and (a == compA or a == compB):
-                            self.minSeps[a] = i
-                        if i < self.minSeps[b] and (b == compA or b == compB):
-                            self.minSeps[b] = i
-
-                    self.compHistory.append([a,b])
-                    if self.last:
-                        if a in self.last or b in self.last:
-                            self.dupCount += 1
-                    self.last = ((a, b))
+                    self.record([a, b])
+                    
                     if self.level > 0:
                         self.lookup[a][b]:bool = res
                         self.lookup[b][a]:bool = not res
@@ -113,10 +108,11 @@ class Comparator:
         """gets the latent score of a given index"""
         if self.rand:
             if index < len(self.objects) // 2: # index is neg dist
-                aScore = self.neg[index]
+                score = self.neg[index]
+                return score, False
             else: #index is pos dist
-                aScore = self.pos[index - len(self.objects) // 2]
-            return aScore
+                score = self.pos[index - len(self.objects) // 2]
+                return score, True
 
     def genLookup(self, objects: list):
         """generate the lookup table and statistics for each object provided"""
@@ -130,7 +126,10 @@ class Comparator:
             from os import getpid, uname
             from time import time
             # get a random seed for each node and each process on that node, and the time
-            np.random.seed(int(str(ord(uname()[1][-1])) + str(getpid()) + str(int(time()))) % 2**31)
+            if self.seed == None:
+                np.random.seed(int(str(ord(uname()[1][-1])) + str(getpid()) + str(int(time()))) % 2**31)
+            else:
+                np.random.seed(self.seed)
             self.neg = list(np.random.normal(size=len(objects)//2,loc=0))
             self.pos = list(np.random.normal(size=len(objects)//2 + len(objects) % 2,loc=1.7))
     
@@ -139,10 +138,10 @@ class Comparator:
         if hasattr(self, "objects"):
             self.compHistory = list()
             self.last = None
-            self.dupCount = None
+            self.dupCount = 0
             for object in self.objects:
                 self.counts[object] = 0
-                self.minSeps[object] = 2*len(objects)
+                self.minSeps[object] = 2*len(self.objects)
 
     def learn(self, arr: list):
         """learn the order of the array provided, assuming the current optimization level allows it"""
@@ -153,6 +152,38 @@ class Comparator:
                     self.lookup[b][a] = False
                     if self.level > 2:
                         Comparator.optimize(self.objects, self.lookup, True, a, b)
+
+    def max(self, arr) -> (int, int):
+        if len(arr) == 0:
+            raise NotImplementedError("I can't take the max of nothing")
+        self.record(arr)
+        maxVal = arr[0]
+        maxScore = self.getLatentScore(arr[0]) if self.rand else arr[0]
+        maxInd = 0
+        for i, imageID in enumerate(arr[1:]):
+            score = self.getLatentScore(imageID) if self.rand else arr[i + 1]
+            if score > maxScore:
+                maxInd = i + 1
+                maxVal = imageID
+                maxScore = score
+
+        return maxInd, maxVal
+
+    def min(self, arr) -> (int, int):
+        if len(arr) == 0:
+            raise NotImplementedError("I can't take the min of nothing")
+        self.record(arr)
+        minVal = arr[0]
+        minScore = self.getLatentScore(arr[0]) if self.rand else arr[0]
+        minInd = 0
+        for i, imageID in enumerate(arr[1:]):
+            score = self.getLatentScore(imageID) if self.rand else arr[i + 1]
+            if score < minScore:
+                minInd = i + 1
+                minVal = imageID
+                minScore = score
+
+        return minInd, minVal
 
     @staticmethod
     def optimize(objects: list, lookup: dict, res: bool, a, b):
@@ -171,7 +202,7 @@ class Comparator:
         return 0
 
 if __name__ == "__main__":
-    test = 3
+    test = 4
     if test == 1:
         comp = Comparator([i for i in range(10)], 2)
         print(comp.compare(0, 1))
@@ -195,5 +226,16 @@ if __name__ == "__main__":
             for obj in objects:
                 print(f"Image number {obj} has latent score {comp.getLatentScore(obj)}")
             print(objects)
-            for arr, stats in mergeSort(objects, comp):
+            for arr, stats in mergeSort(objects, comp, retStats=True):
                 print(arr)
+    elif test == 4:
+        comp = Comparator([*range(8)], rand=True, seed=15)
+        group1 = [0, 1, 2, 3]
+        for i in range(8):
+            print(i, comp.getLatentScore(i))
+        print(comp.max(group1))
+        print(comp.min(group1))
+        group2 = [4, 5, 6, 7]
+        print(comp.max(group2))
+        print(comp.min(group2))
+        print(comp.counts)
--- DylMerger.py
+++ DylMerger.py
@@ -1,5 +1,5 @@
 
-class Merger:
+class Merger(MultiMerger):
     """A class for merging 2 arrays into 1 array
     Must feed it a comparator object. It can also hold onto start/stop indecies if you ask
     'Toggle' parameter defines if you want to do a cocktail merge, as in switch which end you're merging from each call of inc()
@@ -79,3 +79,90 @@ class Merger:
         self.left = self.toggle ^ self.left
         return (self.indexA > self.indexARight) and (self.indexB > self.indexBRight)
         #return (self.outIndex == len(self.output)) or (self.outIndex == self.indexORight)
+
+class MultiMerger:
+    def __init__(self, groups: list, comp, start=0, stop=0, toggle:bool=True):
+        self.groups = [group[:] for group in groups]
+        self.comp = comp
+        self.start = start
+        self.stop = stop
+        self.toggle = toggle
+        self.indecies = [0 for group in groups]
+        self.indeciesRight = [len(group) - 1 for group in groups]
+        self.output = [-1 for i in range(sum([len(group) for group in groups]))]
+        self.OIndex = 0
+        self.OIndexRight = len(self.output) - 1
+        self.left = True
+
+    def inc(self) -> bool:
+        if self.left == True:
+            group = [group[self.indecies[i]] for i, group in enumerate(self.groups)]
+            gI, iI = self.comp.min(group)
+            self.output[self.OIndex] = iI
+            self.indecies[gI] += 1
+            self.OIndex += 1
+        elif self.left == False:
+            group = [group[self.indeciesRight[i]] for i, group in enumerate(self.groups)]
+            gI, iI = self.comp.max(group)
+            self.output[self.OIndexRight] = iI
+            self.indeciesRight[gI] -= 1
+            self.OIndexRight -= 1
+
+
+        # check to see if that group is doneski
+        for i, group in enumerate(self.groups):
+            if self.indecies[i] > self.indeciesRight[i]:
+                self.groups.pop(i)
+                self.indecies.pop(i)
+                self.indeciesRight.pop(i)
+        
+        if len(self.groups) == 1:
+            for i in range(self.indecies[0], self.indeciesRight[0] + 1):
+                self.output[self.OIndex] = self.groups[0][i]
+                self.OIndex += 1
+            return True
+
+        self.left ^= self.toggle
+        return self.OIndex == len(self.output) or len(self.groups) == 0
+
+if __name__ == '__main__':
+    from DylComp import Comparator
+    for test in range(1, 4):
+        if test == 1:
+            objs = [*range(8)]
+            comp = Comparator(objs, rand=False)
+            m = MultiMerger([[0,4], [1, 5], [2, 6], [3, 7]], comp, toggle=True)
+            while not m.inc():
+                pass
+            assert m.output == [0, 1, 2, 3, 4, 5, 6, 7]
+            comp.clearHistory()
+            m1 = MultiMerger([[0,4], [1, 5]], comp, toggle=True)
+            while not m1.inc():
+                pass
+            assert m1.output == [0, 1, 4, 5]
+            m2 = MultiMerger([[2, 6], [3, 7]], comp, toggle=True)
+            while not m2.inc():
+                pass
+            assert m2.output == [2, 3, 6, 7]
+            m3 = MultiMerger([m1.output,m2.output], comp, toggle=True)
+            while not m3.inc():
+                pass
+            assert m3.output == m.output
+
+        elif test == 2:
+            comp = Comparator([0, 1, 2, 3, 4, 5, 6, 7])
+            m = MultiMerger([[0, 3, 5], [1, 2, 4]], comp, toggle=False)
+            while not m.inc():
+                pass
+            assert m.output == [0, 1, 2, 3, 4, 5]
+            m = MultiMerger([[0, 3, 5], [1, 2, 4]], comp, toggle=True)
+            while not m.inc():
+                pass
+            assert m.output == [0, 1, 2, 3, 4, 5]
+
+        elif test == 3:
+            comp = Comparator([0, 1, 2, 3, 4, 5, 6, 7])
+            m = MultiMerger([[0, 1, 2, 3, 5, 6, 7], [4]], comp, toggle=True)
+            while not m.inc():
+                pass
+            assert m.output == [0, 1, 2, 3, 4, 5, 6, 7]
